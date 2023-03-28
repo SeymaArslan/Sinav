7ac4956fc4f02e335d3d546c0da7246a7968a3f6
SeymaArslan <seymarslannn@gmail.com>
2023-03-28 10:33:40 +0300

    Initial Commit

---
+++ Sinav/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sinav
+//
+//  Created by Seyma on 28.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
